c17908f0042620a96df785f65bea80fc0b807ee1
Crowdbotics <team@crowdbotics.com>
2021-08-12 01:53:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_field_29745Identifier",
+  url: "https://frosty_field_29745Identifier.botics.co",
+  api: "https://frosty_field_29745Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
